50d52538b18c32062f0ab5e53e8b916ee0e9e3b2
Nikita Rudometov <nikitarudometof@gmail.com>
2023-12-23 11:52:58 +0700

    feat: max count of tasks done

--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -11,9 +11,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let exceptInd = [];
+  arr.forEach((item, index) => {
+    if (item === -1) exceptInd.push(index);
+  });
+  arr.sort((a, b) => a - b);
+  arr = arr.filter(item => item !== -1);
+
+  exceptInd.forEach(num => {
+    arr.splice(num, 0, -1);
+  })
+
+  return arr;
 }
 
 module.exports = {
--- src/transform-array.js
+++ src/transform-array.js
@@ -15,49 +15,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function transform(arr) {
-  throw new NotImplementedError('Not implemented');
-  // if (Array.isArray(arr)) {
-  //   for (let i = 0; i < arr.length; i++) {
-  //     if (arr[i] === '--double-prev') {
-  //       if (typeof arr[i - 1] !== 'undefined') {
-  //         arr[i] = arr[i - 1];
-  //       } else {
-  //         arr.splice(i, 1);
-  //         i = i - 1;
-  //       }
-  //     }
-  //     if (arr[i] === '--discard-prev') {
-  //       if (typeof arr[i - 1] !== 'undefined') {
-  //         arr.splice(i, 1);
-  //         arr.splice(i - 1, 1);
-  //         i = i - 2;
-  //       } else {
-  //         arr.splice(i, 1);
-  //         i = i - 1;
-  //       }
-  //     }
-  //     if (arr[i] === '--discard-next') {
-  //       if (typeof arr[i + 1] !== 'undefined') {
-  //         arr.splice(i, 2);
-  //         i = i - 2;
-  //       } else {
-  //         arr.splice(i, 1);
-  //         i = i - 1;
-  //       }
-  //     }
-  //     if (arr[i] === '--double-next') {
-  //       if (typeof arr[i + 1] !== 'undefined') {
-  //         arr[i] = arr[i + 1];
-  //       } else {
-  //         arr.splice(i, 1);
-  //         i = i - 1;
-  //       }
-  //     }
-  //   }
-  //   return arr;
-  // } else {
-  //   throw new Error('\'arr\' parameter must be an instance of the Array!');
-  // }
+  if (Array.isArray(arr)) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === '--double-prev') {
+        if (typeof arr[i - 1] !== 'undefined') {
+          arr[i] = arr[i - 1];
+        } else {
+          arr.splice(i, 1);
+          i = i - 1;
+        }
+      }
+      if (arr[i] === '--discard-prev') {
+        if (typeof arr[i - 1] !== 'undefined') {
+          arr.splice(i, 1);
+          arr.splice(i - 1, 1);
+          i = i - 2;
+        } else {
+          arr.splice(i, 1);
+          i = i - 1;
+        }
+      }
+      if (arr[i] === '--discard-next') {
+        if (typeof arr[i + 1] !== 'undefined') {
+          arr.splice(i, 2);
+          i = i - 2;
+        } else {
+          arr.splice(i, 1);
+          i = i - 1;
+        }
+      }
+      if (arr[i] === '--double-next') {
+        if (typeof arr[i + 1] !== 'undefined') {
+          arr[i] = arr[i + 1];
+        } else {
+          arr.splice(i, 1);
+          i = i - 1;
+        }
+      }
+    }
+    return arr;
+  } else {
+    throw new Error('\'arr\' parameter must be an instance of the Array!');
+  }
 }
 
 module.exports = {
